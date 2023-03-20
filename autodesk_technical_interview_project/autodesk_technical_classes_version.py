c0dc758f81c09a929d0cf4c3fc83afc130420b10
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-03-20 13:44:47 -0500

    change status method finished and functioning

--- autodesk_technical_interview_project/autodesk_technical_classes_version.py
+++ autodesk_technical_interview_project/autodesk_technical_classes_version.py
@@ -1,3 +1,5 @@
+import time
+
 class LibraryInventory:
     def __init__(self):
         self.inventory = {}
@@ -29,26 +31,63 @@ class LibraryInventory:
         print(f"\nHere is your inventory with your recent addition: {self.inventory}\n")
 
 
-    def remove_book(self, title):
-        print(f"Here is your book inventory: {self.inventory}")
-        book_to_delete = input("\nWhat book would you like to delete from your inventory?: ").upper()
+    def remove_book(self):
+        print(f"\nHere is your book inventory: {self.inventory}")
+        book_to_delete = None
+        # check for empty inventory; if inventory is empty, user sent to main menu
         if len(self.inventory) == 0:
-            menu_or_quit = input("\nYour inventory is empty! Press 'm' to go back to the main menu or press 'q' to quit: ")
-            # if menu_or_quit == 'm':
-                
-        elif book_to_delete not in self.inventory:
-            print("Hmm... It looks like that books doesn't exist in your inventory.")
+            print("\nHmm... It looks like your inventory is empty.")
+            time.sleep(3)
+            return
+
+        book_to_delete = input("\nWhat book would you like to delete from your inventory?: ").upper()   
+        # check to ensure book is present within inventory; if book not present, user sent to main menu  
+        if book_to_delete not in self.inventory:
+            print("\nHmm... It looks like that book doesn't exist in your inventory.")
+            time.sleep(3)
+            return
+        
+        delete_prompt_answer = input(f"\nAre you sure you want to delete '{book_to_delete}' from your inventory (y or n)?: ")
+        if delete_prompt_answer == 'y':
+            del self.inventory[book_to_delete]
         else:
-            print(f"Here is your book inventory: {self.inventory}")
-            book_to_delete = input("\nWhat book would you like to delete from your inventory?: ")
-            if book_to_delete in self.inventory == True:
-                title = book_to_delete
-                delete_prompt_answer = input(f"Are you sure you want to delete '{title}' from your inventory (y or n)?")
-                if delete_prompt_answer == 'y':
-                    del self.inventory[title]
-                # else:
+            return
+        
+    def change_status(self):
+        print(f"\nHere is your book inventory: {self.inventory}")
+        book_to_change = None
+        # check for empty inventory; if inventory is empty, user sent to main menu
+        if len(self.inventory) == 0:
+            print("\nHmm... It looks like your inventory is empty.")
+            time.sleep(3)
+            return
 
+        book_to_change = input("\nWhat book would you like to change the status of?: ").upper()   
+        # check to ensure book is present within inventory; if book not present, user sent to main menu  
+        if book_to_change not in self.inventory:
+            print("\nHmm... It looks like that book doesn't exist in your inventory.")
+            time.sleep(3)
+            return
+        
+        change_status_prompt_answer = input(f"\nAre you sure you want to change the status of '{book_to_change}' from your inventory (y or n)?: ")
+        if change_status_prompt_answer == 'y':
+            # check for invalid num input
+            invalid_nums = [0, 3, 4, 5, 6, 7, 8, 9]
+            number_pressed = int(input("\nEnter 1 to mark book as 'on shelf' and 2 to mark as 'checked out': "))
+            if number_pressed in invalid_nums:
+                while number_pressed != 1 and number_pressed != 2:
+                    print("\nOOPS! You entered the wrong number please try again.")
+                    number_pressed = int(input("\nEnter 1 to mark book as 'on shelf' and 2 to mark as 'checked out': "))
 
+            if number_pressed == 1:
+                status = "on shelf"
+            else:
+                status = "checked out"
+            
+            self.inventory[book_to_change] = status
+            print(f"Your new inventory is:")
+        else:
+            return
         
 
 
@@ -68,12 +107,12 @@ def main():
             4. View your list of checked out books.\n\
             5. Exit\n")
         
-        # 1. Add a book
-        if number_pressed == '1':
+        if number_pressed == '1':            # 1. Option to add a book
             library_inventory.new_book()
-        elif number_pressed == '2':
+        elif number_pressed == '2':          # 1. Option to remove a book
             library_inventory.remove_book()
-        # elif number_pressed == '3':
+        elif number_pressed == '3':
+            library_inventory.change_status()
 
     
 
